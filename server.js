e407f336318fafaafa143eb6164b152e31cc9a4c
Ben Johansen <johansenben12@gmail.com>
2026-05-18 07:49:29 -0600

    basic express.js setup

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressLayouts = require("express-ejs-layouts");
+
+//********** init **********
+const app = express();
+const PORT = 3000;
+//********** middleware **********
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+
+app.get("/login", (req,res) => {
+  res.render("login", {
+    title: "Login",
+    layout: "layout"
+  });
+});
+app.post("/login", (req,res) => {
+  const { username, password } = req.body;
+  res.send(`login post: username: ${username}, password: ${password}`); //todo
+});
+
+app.get("/first-new-user", (req,res) => {
+  res.render("first-new-user", {
+    title: "Create New Admin",
+    layout: "layout"
+  });
+});
+app.post("/first-new-user", (req,res) => {
+  const { username, password, passwordConfirmation } = req.body;
+  res.send(`login post: username: ${username}, password: ${password}, password confirmation: ${passwordConfirmation}`); //todo
+});
+
+//********** start **********
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Docker Manager server running on port ${PORT}`);
+});
