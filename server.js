02f5a132c56d23a3bd8c698e2505f3eb9a0355ab
Eric Evijay Ohiani <ericeviayohiani@gmail.com>
2026-05-19 04:57:30 +0100

    Eric: Configured public directory middleware in server.js

--- server.js
+++ server.js
@@ -5,32 +5,38 @@ const expressLayouts = require("express-ejs-layouts");
 //********** init **********
 const app = express();
 const PORT = 3000;
-//********** middleware **********
 
+//********** middleware **********
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressLayouts);
+
+// Serve static files (CSS, images, client-side JS) from the 'public' folder
+app.use(express.static("public")); 
+
 app.set("view engine", "ejs");
 
-
-app.get("/login", (req,res) => {
+//********** routes **********
+app.get("/login", (req, res) => {
   res.render("login", {
     title: "Login",
     layout: "layout"
   });
 });
-app.post("/login", (req,res) => {
+
+app.post("/login", (req, res) => {
   const { username, password } = req.body;
   res.send(`login post: username: ${username}, password: ${password}`); //todo
 });
 
-app.get("/first-new-user", (req,res) => {
+app.get("/first-new-user", (req, res) => {
   res.render("first-new-user", {
     title: "Create New Admin",
     layout: "layout"
   });
 });
-app.post("/first-new-user", (req,res) => {
+
+app.post("/first-new-user", (req, res) => {
   const { username, password, passwordConfirmation } = req.body;
   res.send(`login post: username: ${username}, password: ${password}, password confirmation: ${passwordConfirmation}`); //todo
 });
